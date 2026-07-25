1e7204b7c28ea6ce070fec429d5a054205caac30
Priyanshu Kumar Sharma <24051800@kiit.ac.in>
2026-07-25 11:29:45 +0530

    Health checking part implemented,phase 1 completed

--- core-backend/src/main/java/com/aegissight/health/api/controller/HealthController.java
+++ core-backend/src/main/java/com/aegissight/health/api/controller/HealthController.java
@@ -1,4 +1,23 @@
 package com.aegissight.health.api.controller;
 
+import com.aegissight.health.api.dto.HealthResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+
+@RestController
+@RequestMapping("/api/health")
 public class HealthController {
+
+
+    @GetMapping
+    public HealthResponse health(){
+
+        return new HealthResponse(
+                "UP",
+                Instant.now()
+        );
+    }
 }
--- core-backend/src/main/java/com/aegissight/health/api/dto/HealthResponse.java
+++ core-backend/src/main/java/com/aegissight/health/api/dto/HealthResponse.java
@@ -3,8 +3,6 @@ package com.aegissight.health.api.dto;
 import java.time.Instant;
 
 public record HealthResponse(
-    String status,
-    String service,
-    String version,
-    Instant timestamp
+        String status,
+        Instant timestamp
 ) {}
